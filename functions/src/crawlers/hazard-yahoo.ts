b827280e8d0ca6565d2b3ee3de069960bf928c4f
yamam00s <s.yamamo0327@gmail.com>
2020-05-04 05:45:02 +0900

    create スケジューラー

--- functions/src/crawlers/hazard-yahoo.ts
+++ functions/src/crawlers/hazard-yahoo.ts
@@ -1,29 +1,34 @@
-import { Page } from 'playwright'
+import { chromium } from 'playwright'
 import { Region, blankRegion } from '../services/models/region'
 
-const feedHazard = async (page: Page) => {
-    const url: string = 'https://hazard.yahoo.co.jp/article/20200207'
-    await page.goto(url)
-    const mapSel = '#map .table .table__section div'
-    const items = await page.$$(mapSel)
-    let feedData: Region[] = []
+const feedHazard = async () => {
+  const browser = await chromium.launch()
+  const context = await browser.newContext()
+  const page = await context.newPage()
+  const url: string = 'https://hazard.yahoo.co.jp/article/20200207'
+  await page.goto(url)
+  const mapSel = '#map .table .table__section div'
+  const items = await page.$$(mapSel)
+  let feedData: Region[] = []
 
-    for await (const [index, item] of items.entries()) {
-      const regionName = await item.$eval('dt', el => el.textContent)
-      const todayValue = await item.$eval('dd', el => el.textContent)
+  for await (const [index, item] of items.entries()) {
+    const regionName = await item.$eval('dt', el => el.textContent)
+    const todayValue = await item.$eval('dd', el => el.textContent)
 
-      if (regionName && todayValue && index !== 0) {
-        const todayNumber = parseInt(todayValue, 10) || 0
-        feedData.push({
-          ...blankRegion,
-          id: index,
-          name: regionName,
-          todayInfection: todayNumber
-        })
-      }
+    if (regionName && todayValue && index !== 0) {
+      const todayNumber = parseInt(todayValue, 10) || 0
+      feedData.push({
+        ...blankRegion,
+        id: index,
+        name: regionName,
+        todayInfection: todayNumber
+      })
     }
+  }
 
-    return feedData
+  await browser.close()
+
+  return feedData
 }
 
 export default feedHazard
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,20 @@
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
 import * as functions from 'firebase-functions';
+import admin from 'firebase-admin';
+import feedHazard from 'crawlers/hazard-yahoo'
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const fetchHazard = functions
+  .region('asia-northeast1')
+  .runWith({
+    timeoutSeconds: 300,
+    memory: '2GB',
+  })
+  .pubsub.schedule('0 0 * * *')
+  .timeZone('Asia/Tokyo')
+  .onRun(async () => {
+    await feedHazard()
+  })
+
