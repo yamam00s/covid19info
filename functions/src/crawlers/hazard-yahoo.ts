09d6130a2747544ef1db5cef5a7d7077d9de65c0
yamam00s <s.yamamo0327@gmail.com>
2020-05-05 08:06:03 +0900

    playwright→puppeteer

--- functions/src/crawlers/hazard-yahoo.ts
+++ functions/src/crawlers/hazard-yahoo.ts
@@ -1,10 +1,23 @@
-import { chromium, errors } from 'playwright'
+// import { chromium, errors } from 'playwright'
+import puppeteer, { errors } from 'puppeteer';
+
 import { Hazard, blankHazard } from '../services/models/hazard'
 
 const feedHazard = async () => {
-  const browser = await chromium.launch()
-  const context = await browser.newContext()
-  const page = await context.newPage()
+  const browser = await puppeteer.launch({
+    args: [
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--disable-setuid-sandbox',
+      '--no-first-run',
+      '--no-sandbox',
+      '--no-zygote',
+      '--single-process'
+    ],
+    headless: true
+  })
+  // const context = await browser.newContext()
+  const page = await browser.newPage()
   const url: string = 'https://hazard.yahoo.co.jp/article'
   await page.goto(`${url}/20200207`)
   const mapSel = '#map .table .table__section div'
@@ -16,9 +29,9 @@ const feedHazard = async () => {
       if (index === 0) continue
       const regionName = await item.$eval('dt', el => el.textContent)
       const todayValue = await item.$eval('dd', el => el.textContent)
-      const link = await item.$eval('a', el => el.href)
-      const keyValue = link.replace(`${url}/covid19`, '')
-      if (regionName && todayValue) {
+      const link = await item.$eval('a', el => el.getAttribute('href'))
+      const keyValue = link?.replace(`${url}/covid19`, '')
+      if (regionName && todayValue && keyValue) {
         const todayNumber = parseInt(todayValue, 10) || 0
         feedData.push({
           ...blankHazard,
