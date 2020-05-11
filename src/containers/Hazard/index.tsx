cf6ee13295c920b4de79ff31771ee3036f0e873e
yamam00s <s.yamamo0327@gmail.com>
2020-05-12 05:24:50 +0900

    selectで表示する地域を変更

--- src/containers/Hazard/index.tsx
+++ src/containers/Hazard/index.tsx
@@ -1,15 +1,29 @@
 import React, { FC, useState, useEffect } from 'react'
 import { ClientHazard } from 'services/models/hazard';
 import HazardMain from 'components/Hazard'
+import SelectForm from 'components/Hazard/SelectForm'
 
 const HazardContainer: FC<{ hazards: ClientHazard[] }>= ({ hazards })=> {
   const [hazard, setHazard] = useState<ClientHazard>(hazards[0])
+  const tokyo = [...hazards].find(hazard => hazard.key === 'tokyo')
+
+  const handleChange = (event: React.ChangeEvent<{ value: string }>) => {
+    const selected = hazards.find(hazard => hazard.key === event.target.value)
+    setHazard(selected)
+  }
 
   useEffect(() => {
-    setHazard(hazards[0])
+    setHazard(tokyo)
   }, [])
 
-  return <HazardMain hazard={hazard} />
+  const selectForm = { hazards, value: hazard.key, handleChange }
+
+  return (
+    <>
+      <SelectForm selectFormProps={selectForm} />
+      <HazardMain hazard={hazard} />
+    </>
+  )
 }
 
 export default HazardContainer
