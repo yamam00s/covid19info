163818cc247b07ec5ef711c5b4d6666d63b1506c
yamam00s <s.yamamo0327@gmail.com>
2020-05-15 01:08:17 +0900

    config解禁

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyARciFr39z4kToiQx07vSIg8iXbiJsGBBo",
+  authDomain: "covid19info-574cd.firebaseapp.com",
+  databaseURL: "https://covid19info-574cd.firebaseio.com",
+  projectId: "covid19info-574cd",
+  storageBucket: "covid19info-574cd.appspot.com",
+  messagingSenderId: "997972210419",
+  appId: "1:997972210419:web:48818888370752fc7cfd86"
+} as const
+
+export default firebaseConfig
